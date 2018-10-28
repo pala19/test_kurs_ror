f98cbb96c1a694cd182e0ab75cef162ad6ee6eef
pala19 <p.biruta19@gmail.com>
2018-10-28 18:26:13 +0100

    fix mistakes and modify ignored files

--- app/models/restorer.rb
+++ app/models/restorer.rb
@@ -1,10 +1,6 @@
 class Restorer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   has_many :restaurants
