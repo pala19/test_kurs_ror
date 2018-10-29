d26f33e6e471ea696e4ea76fe296f12d467657e7
pala19 <p.biruta19@gmail.com>
2018-10-29 18:48:49 +0100

    add misplaced comma

--- app/models/restorer.rb
+++ app/models/restorer.rb
@@ -3,7 +3,7 @@
 class Restorer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :restaurants
 end
