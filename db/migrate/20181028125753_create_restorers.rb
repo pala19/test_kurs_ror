4087fe8344ccea30a05466ff45ba756a4b0ce7cc
pala19 <p.biruta19@gmail.com>
2018-10-29 19:14:21 +0100

    fixes in db files

--- db/migrate/20181028125753_create_restorers.rb
+++ db/migrate/20181028125753_create_restorers.rb
@@ -4,9 +4,6 @@ class CreateRestorers < ActiveRecord::Migration[5.2]
   def change
     create_table :restorers do |t|
       t.string :name
-      t.string :email
-      t.string :password
-
       t.timestamps
     end
   end
