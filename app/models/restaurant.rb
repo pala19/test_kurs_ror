3c7ec0b02e401afb7938368cb38bac959566853a
pala19 <p.biruta19@gmail.com>
2018-11-04 14:52:00 +0100

    Add connection restorer-restaurant to admin panel

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
+  belongs_to :restorer
 end
---
+++ db/migrate/20181104133714_add_restorer_id_to_restaurant.rb
@@ -0,0 +1,5 @@
+class AddRestorerIdToRestaurant < ActiveRecord::Migration[5.2]
+  def change
+    add_column :restaurants, :restorer_id, :integer
+  end
+end
