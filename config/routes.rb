ceba9f6e2f7c4a743cd08efe81c1b3ed6483f4cc
Alicja Gustowska <alicja.gustowska@gmail.com>
2018-11-11 18:39:06 +0100

    Change default page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   ActiveAdmin.routes(self)
-  root to: 'pages#home'
+  root to: 'restaurants#index'
 
   namespace :restorer_panel do # , path: 'restorer' do
     resources :restorer
