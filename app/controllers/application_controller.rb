4306efa8ade9071b0f15c52e5aa58c04680825f1
pala19 <p.biruta19@gmail.com>
2018-12-10 17:55:36 +0100

    Add name to registration and edit profile to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:surname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:surname, :name])
   end
 end
